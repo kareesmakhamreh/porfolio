00adc5ba13188828f81e3f1ff5b26bdc65f2f413
kareesmakhamreh <86370884+kareesmakhamreh@users.noreply.github.com>
2024-11-04 14:23:21 +0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,5 +1,3 @@
-// You can add JavaScript functionality if needed
-// For example, adding smooth scrolling for navigation links
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function(e) {
         e.preventDefault();
